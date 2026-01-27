ea2c37c612026395b876f915d05eb392de9c14cd
Sanduni <sanduni2001@gmail.com>
2026-01-27 14:31:13 +0530

    final production settings fixes

--- config/config/urls.py
+++ config/config/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from shop.views import home  
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
