fd70b60d5450ac4c8a7e41da7a5ed713893096e1
Sanduni <sanduni2001@gmail.com>
2026-02-04 21:54:13 +0530

    remove auto admin

--- config/config/settings.py
+++ config/config/settings.py
@@ -274,16 +274,3 @@ AXES_COOLOFF_TIME = 1           # 1 hour lock
 AXES_LOCK_OUT_AT_FAILURE = True
 AXES_RESET_ON_SUCCESS = True
 
-import django
-django.setup()
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        "admin",
-        "admin@gmail.com",
-        "admin1234"
-    )
