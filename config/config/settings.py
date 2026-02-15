b4c620c05b36fa02096533007577ccfb3489134b
Sanduni <sanduni2001@gmail.com>
2026-02-15 14:02:16 +0530

    fix static dirs

--- config/config/settings.py
+++ config/config/settings.py
@@ -83,7 +83,14 @@ DATABASES = {
 }
 
 STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "shop" / "static",
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
